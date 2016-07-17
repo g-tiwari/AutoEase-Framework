4c11723ae189f158d5dfddc39211876e42bd978c
Gaurav Tiwari <gauravtiwari91@yahoo.com>
2016-07-18 00:35:24 +0530

    Added reporting in the framework

--- src/test/java/org/selenium2automate/framework/testrunner/CucumberTestRunner.java
+++ src/test/java/org/selenium2automate/framework/testrunner/CucumberTestRunner.java
@@ -1,5 +1,6 @@
 package org.selenium2automate.framework.testrunner;
 
+import com.cucumber.listener.ExtentCucumberFormatter;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
@@ -12,6 +13,10 @@ import static org.selenium2automate.framework.WebDriverGenerator.getDriver;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "classpath:Test Scenario/",
@@ -21,7 +26,8 @@ import cucumber.api.junit.Cucumber;
 		format={"pretty",
 				"json:target/cucumber-json-report/cucumber.json",
 				"html:target/cucumber-html-report",
-				"rerun:target/cucumber-failed-report/failed-rerun.txt"}
+				"rerun:target/cucumber-failed-report/failed-rerun.txt"},
+		plugin = {"com.cucumber.listener.ExtentCucumberFormatter:output/report.html"}
 		)
 
 //,tags={"@groupTest"}
@@ -38,6 +44,22 @@ public class CucumberTestRunner {
 		System.out.println("*****************************Welcome to****************************");
 		System.out.println("************************Auto-Ease Framework************************");
 		System.out.println("*******************************************************************");
+		// Initiates the extent report and generates the output in the output/Run_<unique timestamp>/report.html file by default.
+		ExtentCucumberFormatter.initiateExtentCucumberFormatter();
+
+		// Loads the extent config xml to customize on the report.
+		ExtentCucumberFormatter.loadConfig(new File("src/test/resources/extent-config.xml"));
+
+		// User can add the system information as follows
+		ExtentCucumberFormatter.addSystemInfo("Browser Name", "Firefox");
+		ExtentCucumberFormatter.addSystemInfo("Browser version", "v31.0");
+		ExtentCucumberFormatter.addSystemInfo("Selenium version", "v2.53.0");
+
+		// Also you can add system information using a hash map
+		Map systemInfo = new HashMap();
+		systemInfo.put("Cucumber version", "v1.2.3");
+		systemInfo.put("Extent Cucumber Reporter version", "v1.1.0");
+		ExtentCucumberFormatter.addSystemInfo(systemInfo);
 	}
 
 	
